266817b5f6dc15437999c4bdb3441d9227f118e7
Nathan Goshay <naterg2000@gmail.com>
2022-10-24 22:54:18 -0700

    updated  home page

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,31 +1,110 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { Text, View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React, { useState } from "react";
+import { Keyboard, KeyboardAvoidingView, Text, Touchable, View } from "react-native";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 import { auth } from "../../firebase/firebase";
+import NavBar from "../../NavBar";
 import { styles } from "./styles";
 
 const HomeScreen = () => {
     const navigation = useNavigation();
 
+    const [searchQuery, setSearchQuery] = useState('');
+
+    //sign the user out
     const handleSignOut = () => {
         auth.signOut().then( () => {
             navigation.replace("Login")
         }).catch(error => alert(error.message));
     }
 
+    //search the database/services for the user's query
+    const searchForUserQuery = () => {
+
+        //for now, just log the query
+        console.log(searchQuery);
+
+        //search for pages
+
+        //serach for resources/programs
+
+        //search for employees
+
+    }
+
     return (
-        <View style = { styles.container }>
-            <Text>Email: { auth.currentUser?.email }</Text>
+        <KeyboardAvoidingView style = { styles.container }>
 
-            <TouchableOpacity style = { styles.button }>
-                <Text
-                    onPress = { handleSignOut }
-                    style = { styles.buttonText }>Sign out
-                </Text>
-            </TouchableOpacity>
+            {/* resources title section */}
+            <View>
+                <Text style={ styles.resourcesHeader }>RESOURCES</Text>
+            </View>
 
-        </View>
+            {/* profile button and search bar section */}
+            <View style={ styles.profileSearchBarDiv }>
+
+                {/* profile button section */}
+                <View style={ styles.profileButtonContainer }>
+                    <TouchableOpacity 
+                        style={ styles.profileButton }
+                        onPress={ () => { }}
+                    >
+                        <Text>Profile</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {/* search bar */}
+                <View style = { styles.searchContainer }>
+                    <TextInput
+                        placeholder = "Search"
+                        placeholderTextColor = 'white'
+                        maxLength = { 100 }
+                        value = { searchQuery }
+                        onChangeText = { text => setSearchQuery(text) }
+                        onSubmitEditing = { searchForUserQuery }
+                        style = { styles.input }
+                    />
+                </View>
+
+            </View>
+
+            {/* sign out section
+            <View style={ styles.container }>
+                <Text>Email: { auth.currentUser?.email }</Text>
+
+                <TouchableOpacity style = { styles.button }>
+                    <Text
+                        onPress = { handleSignOut }
+                        style = { styles.buttonText }>Sign out
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            */}
+
+            {/* favorited categoires section */}
+            <View style={ styles.favoritesSection }>
+                <Text>Favorites section</Text>
+            </View>
+
+            {/* categories grid section */}
+            <View style={ styles.categoriesContainer }>
+                <Text>Categories Section</Text>
+                <View style={ styles.categoriesGrid }>
+
+                    <TouchableOpacity style={ styles.categoryButton }>
+                        <Text>This is a button</Text>
+                    </TouchableOpacity>
+                    
+
+                </View>
+            </View>
+
+            {/* navbar section */}
+            <View style={ styles.navBarDiv}>
+                <NavBar />
+            </View>
+
+        </KeyboardAvoidingView>
     )
 }
 
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,10 +1,40 @@
 import { StyleSheet } from "react-native";
+import { withSafeAreaInsets } from "react-native-safe-area-context";
 
 const styles = StyleSheet.create( {
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
+        marginRight: 10,
+        marginLeft: 10,
+        marginTop: 5,
+        marginBottom: 15,
+    },
+    resourcesHeader: {
+      marginTop: 50,
+      fontWeight: '700',
+      color: '#00BFFF',
+      borderColor: 'red',
+      borderWidth: 1,
+    },
+    profileSearchBarDiv: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    profileButtonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+
+
+        borderColor: 'red',
+        borderWidth: 1,
+    },
+    profileButton: {
+        backgroundColor: 'white',
+        borderColor: 'black',
     },
     button: {
         backgroundColor: '#00BFFF',
@@ -18,6 +48,54 @@ const styles = StyleSheet.create( {
         fontWeight: '700',
         fontSize: 16,
     },
+    searchContainer: {
+        width: '90%',
+        flex: 1,
+        justifyContent: 'flex-start',
+        borderColor: 'green',
+        borderWidth: 1,
+    },
+    input: {
+        backgroundColor: '#00BFFF',
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+        borderRadius: 100,
+        marginTop: 20,
+        color: 'white',
+    },
+    textInput: {
+        color: 'white',
+    },
+    favoritesSection: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        borderColor: 'red',
+        borderWidth: 1,
+    },
+    categoriesContainer: {
+        flex: 1,
+        width: '100%',
+        borderColor: 'red',
+        borderWidth: 1,
+    },
+    categoriesGrid: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        flexGrow: 1,
+        width: '100%',
+        borderColor: 'purple',
+        borderWidth: 1,
+    },
+    categoryButton: {
+        width: 30,
+        height: 40,
+        borderColor: 'green',
+        borderWidth: 1,
+    },
+    navBarDiv: {
+        width: '100%',
+    }
 } );
 
 export { styles };
